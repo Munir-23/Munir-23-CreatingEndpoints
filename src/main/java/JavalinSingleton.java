42af412ffbdd9cda0ed662ece5ac3792cfc805ff
Munir-23 <munirabdi27@gmail.com>
2023-03-05 17:14:03 +0000

    done

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -16,9 +16,12 @@ public class JavalinSingleton {
      */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
+        app.get("/hello", ctx ->{
+            ctx.result( "Hello World");});
+        
         
         //write endpoint here
-
+        
         return app;
     }
     
